64b1497e818d9101de7f0b465ed0ebb60aa03c2b
Jayendra Dey <deyjayendra@gmail.com>
2025-05-19 16:54:06 +0530

    add context api

--- context_api_tusk/src/components/header/Header.jsx
+++ context_api_tusk/src/components/header/Header.jsx
@@ -1,12 +1,13 @@
 import React from "react"
 
 import styles from './Header.module.css';
+import { NavLink } from "react-router-dom";
 
 const Header =()=> {
     return (
         <div className={styles.header_container}>
             <h2>Logo</h2>
-            <p>Home</p>
+            <NavLink style={{color:"white",textDecoration:"none"}} to={"/"}><p>Home</p></NavLink>
             <p>Category</p>
             <p>About Us</p>
         </div>
