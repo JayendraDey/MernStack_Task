4673d955c5ce3d01946ad35faa586cff9a96a94f
Jayendra Dey <deyjayendra@gmail.com>
2025-06-17 20:47:47 +0530

    ......

--- task_35/backend/index.js
+++ task_35/backend/index.js
@@ -4,23 +4,21 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import { userRouter } from "./routers/userRouter.js";
 import { postRouter } from "./routers/postRouter.js";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-dotenv.config()
 
+dotenv.config();
 
 const app = express();
-
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-connectDB(process.env.MONGO_URL)
+connectDB(process.env.MONGO_URL);
 
 app.use("/", userRouter);
 app.use("/", postRouter);
 
-const PORT = 8011;
+const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
